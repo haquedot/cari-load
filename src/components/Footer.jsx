8b05bdf91ce79d02fd845dfa5c76ed528a862cc0
MD MERAJUL HAQUE <113513789+haquedot@users.noreply.github.com>
2024-07-18 15:58:04 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,26 +18,26 @@ const Footer = () => {
                         <ul className="list-unstyled">
                             <li><a href="#" className="text-white">Home</a></li>
                             <li><a href="#" className="text-white">About</a></li>
-                            <li><a href="#" className="text-white">Gallery</a></li>
-                            <li><a href="#" className="text-white">Pricing</a></li>
                             <li><a href="#" className="text-white">Contact</a></li>
+                            <li><a href="#" className="text-white">Blog</a></li>
+    
                         </ul>
                     </div>
                     <div className="col-md-4 mb-3">
                         <h5>Contact Us</h5>
                         <p>
-                            <i className="bi bi-geo-alt-fill"></i> 123 Garhara, Begusarai, Bihar, India
+                            <i className="bi bi-geo-alt-fill"></i> 123 Tankal, Yahuadu
                         </p>
                         <p>
                             <i className="bi bi-telephone-fill"></i> +91 8748734236
                         </p>
                         <p>
-                            <i className="bi bi-envelope-fill"></i> dhorses@gmail.com
+                            <i className="bi bi-envelope-fill"></i> info@cariload.com
                         </p>
                     </div>
                 </div>
                 <div className="text-center pt-5 pb-2">
-                    <p className="mb-0">&copy; 2024 D Horses. All Rights Reserved.</p>
+                    <p className="mb-0">&copy; 2024 CariLoad. All Rights Reserved.</p>
                 </div>
             </div>
         </footer>
